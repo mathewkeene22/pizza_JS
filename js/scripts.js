eb970a6772e20826c7eba566b76caebef27dca66
Mathew Keene <mathewkeene22@gmail.com>
2015-03-28 11:57:51 -0700

    added raw JavaScript to create cost

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var pizza = {
+  size : "";
+  topping : "";
+  price : function() {
+//small
+  if (this.size === 10 && this.topping === "cheese" ) {
+    return 10
+  } else if ( this.size === 10 && this.topping === "pepperoni" ) {
+    return 12
+//medium
+  } else if ( this.size === 14 && this.topping === "cheese" ) {
+    return 13
+  } else if ( this.size === 14 && this.topping === "pepperoni" ) {
+    return 15
+//large
+  } else if ( this.size === 18 && this.topping === "cheese" ) {
+    return 16
+  } else ( this.size === 18 && this.topping === "pepperoni" ) {
+    return 18
+  }
+
+  };
+};
